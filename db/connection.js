0d18c4031f3856ead01a354bb56c161ba1c303a3
Alina <a.08pon@gmail.com>
2024-03-10 20:37:59 +0000

    postgresql hosting

--- db/connection.js
+++ db/connection.js
@@ -5,17 +5,6 @@ const { Pool } = require("pg");
 //   password: "nnn",
 //   database: "drug_shops",
 //   port: 5432,
-//   // connectionString: process.env.DATABASE_URL,
-//   connectionString:
-//     "postgres://yiufbthy:y7cqjTBjhJGez-EN05U3IshmHTGmzMiz@surus.db.elephantsql.com/yiufbthy",
-// };
-
-// const config = {
-//   user: "yiufbthy",
-//   password: "y7cqjTBjhJGez-EN05U3IshmHTGmzMiz",
-//   host: "surus.db.elephantsql.com",
-//   port: 5432,
-//   database: "yiufbthy",
 // };
 
 const config = { connectionString: process.env.DATABASE_URL };
