7f32b3a8d8e884b31a7659c9f49ade680088c40c
Alina <a.08pon@gmail.com>
2024-03-10 20:34:12 +0000

    postgresql hosting

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,21 @@
 const { Pool } = require("pg");
 
-let config = {
-  user: "alina",
-  password: "nnn",
-  database: "drug_shops",
+// let config = {
+//   user: "alina",
+//   password: "nnn",
+//   database: "drug_shops",
+//   port: 5432,
+//   // connectionString: process.env.DATABASE_URL,
+//   connectionString:
+//     "postgres://yiufbthy:y7cqjTBjhJGez-EN05U3IshmHTGmzMiz@surus.db.elephantsql.com/yiufbthy",
+// };
+
+const config = {
+  user: "yiufbthy",
+  password: "y7cqjTBjhJGez-EN05U3IshmHTGmzMiz",
+  host: "surus.db.elephantsql.com",
   port: 5432,
-  // connectionString: process.env.DATABASE_URL,
-  connectionString:
-    "postgres://yiufbthy:y7cqjTBjhJGez-EN05U3IshmHTGmzMiz@surus.db.elephantsql.com/yiufbthy",
+  database: "yiufbthy",
 };
 
 const pool = new Pool(config);
