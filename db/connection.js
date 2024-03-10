cbabd76934ea4eef01e734c8a13f7df57e522a20
Alina <a.08pon@gmail.com>
2024-03-10 19:33:00 +0000

    added dotenv file

--- db/connection.js
+++ db/connection.js
@@ -4,11 +4,18 @@ const { Pool } = require("pg");
 //   throw new Error("PGDATABASE not set!");
 // }
 
-const pool = new Pool({
+let config = {
   user: "alina",
   password: "nnn",
   database: "drug_shops",
   port: 5432,
-});
+};
+const ENV = process.env.NODE_ENV || "development";
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+const pool = new Pool(config);
 
 module.exports = pool;
