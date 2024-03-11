ba7056b1397f99940e6fa5753dd5e0c8a0c498e7
Alina <a.08pon@gmail.com>
2024-03-11 00:13:09 +0000

    added proxyMiddleware

--- app.js
+++ app.js
@@ -18,13 +18,23 @@ const {
 const app = express();
 
 app.use(express.json());
-app.use(
-  "/api",
-  createProxyMiddleware({
-    target: "https://molchanova-drug-shop.onrender.com",
-    changeOrigin: true,
-  })
-);
+// app.use(
+//   "/api",
+//   createProxyMiddleware({
+//     target: "https://molchanova-drug-shop.onrender.com",
+//     changeOrigin: true,
+//   })
+// );
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
 
 app.get("/", showEndpoints);
 app.get("/drugs", getAll);
